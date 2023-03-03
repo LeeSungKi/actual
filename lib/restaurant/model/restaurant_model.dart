22b988f88aea8e913fa765e9da5b27017e5bb706
leesungki <leesunggi92@gmail.com>
2023-03-03 20:06:57 +0900

    Initial commit

---
+++ lib/restaurant/model/restaurant_model.dart
@@ -0,0 +1,48 @@
+import '../../common/const/data.dart';
+
+enum RestaurantPriceRange {
+  expensive,
+  medium,
+  cheap,
+}
+
+class RestaruantModel {
+  final String id;
+  final String name;
+  final String thumbUrl;
+  final List<String> tags;
+  final RestaurantPriceRange priceRange;
+  final double ratings;
+  final int ratingsCount;
+  final int deliveryTime;
+  final int deliveryFee;
+
+  RestaruantModel({
+    required this.id,
+    required this.name,
+    required this.thumbUrl,
+    required this.tags,
+    required this.priceRange,
+    required this.ratings,
+    required this.ratingsCount,
+    required this.deliveryTime,
+    required this.deliveryFee,
+  });
+
+  factory RestaruantModel.fromJson({
+    required Map<String, dynamic> json,
+  }) {
+    return RestaruantModel(
+      id: json['id'],
+      name: json['name'],
+      thumbUrl: 'http://$ip${json['thumbUrl']}',
+      tags: List<String>.from(json['tags']),
+      priceRange: RestaurantPriceRange.values
+          .firstWhere((e) => e.name == json['priceRange']),
+      ratings: json['ratings'],
+      ratingsCount: json['ratingsCount'],
+      deliveryTime: json['deliveryTime'],
+      deliveryFee: json['deliveryFee'],
+    );
+  }
+}
